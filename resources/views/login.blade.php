456eef7194baeccf96aeee490232befba20b3e87
chrisvanlier2005 <93382176+chrisvanlier2005@users.noreply.github.com>
2024-12-15 12:53:16 +0100

    install daisyui and style the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,21 +1,68 @@
-<form action="{{ route('login.store') }}" method="post">
-    @csrf
+<x-layouts.main class="">
+    <main class="flex items-center justify-center min-h-screen">
+        <div class="card shadow-2xl bg-base-200 max-w-3xl w-full mx-auto border border-base-300">
+            <form action="{{ route('login.store') }}" method="post">
+                @csrf
+                <div class="card-body space-y-4">
+                    <h1 class="text-3xl font-semibold text-center">
+                        Sign in to your account
+                    </h1>
 
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" required>
+                    <div class="form-control">
+                        <label for="email" class="label">
+                            <span class="label-text">
+                                Email
+                            </span>
+                        </label>
 
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" required>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            class="input input-bordered"
+                            required
+                        >
+                    </div>
 
-    @error('email')
-        <div>{{ $message }}</div>
-    @enderror
+                    <div class="form-control">
+                        <label for="password" class="label">
+                            <span class="label-text">
+                                Password
+                            </span>
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            class="input input-bordered"
+                            required
+                        >
+                    </div>
 
-    @error('password')
-        <div>{{ $message }}</div>
-    @enderror
+                    <div class="form-control">
+                        <label class="label cursor-pointer">
+                            <span class="label-text">Remember me</span>
+                            <input type="checkbox" checked="checked" class="checkbox" />
+                        </label>
+                    </div>
 
-    <button type="submit">Login</button>
-</form>
+                    @error('email')
+                    <span class="text-error">{{ $message }}</span>
+                    @enderror
 
-@dump(auth()->user())
+                    @error('password')
+                    <div class="text-error">{{ $message }}</div>
+                    @enderror
+
+                    <button type="submit" class="btn btn-primary">Login</button>
+
+                    <p class="text-gray-500 dark:text-gray-400 text-center">
+                        <a href="{{ route('register.create') }}" class="link">
+                            Don't have an account? Register here
+                        </a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </main>
+</x-layouts.main>
